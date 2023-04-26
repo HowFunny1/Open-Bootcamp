cab58bb77f74ed60757fc59500539746cfde2ac8
HowFunny <HowFunny2.0@hotmail.com>
2023-04-26 14:28:09 -0400

    Create ejerciciostema1.java

---
+++ Curso de Java Básico/ejerciciostema1/ejerciciostema1/ejerciciostema1.java
@@ -0,0 +1,54 @@
+package ejerciciostema1;
+
+public class ejerciciostema1 {
+
+    public static void main(String[] args) {
+        int i = 0;
+        long l = 0;
+        double d = 1;
+        boolean b = false;
+        String s = "texto";
+
+        System.out.println(i + " " + l + " " + d + " " +b + " " + s);
+    }
+}
+
+//respuesta 
+
+/*
+ package com.example;
+
+public class ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // enteros
+        byte number1 = 1; // 1 byte
+        short number2 = 10; // 2 bytes
+        int number3 = 50; // 4 bytes
+        long number4 = 100; // 8 bytes
+
+        System.out.println("Numeros enteros: " + number1 + " , " + number2 + " , " + number3 + " , " + number4);
+
+        // decimales
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        System.out.println("Decimales: " + decimal1 + " , " + decimal2);
+
+        // cadena de texto
+        String nombre = "Jose David";
+        String apellido = "Prieto";
+
+        // caracter
+        char caracter1 = 'a';
+
+        System.out.println("Cadena de texto: " + nombre + " " + apellido + "\nCaracter: " + caracter1);
+
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+    }
+
+}
+*/
