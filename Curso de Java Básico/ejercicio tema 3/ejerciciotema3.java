087f7fb41cb011051288667c9944e98067960a21
HowFunny <HowFunny2.0@hotmail.com>
2023-04-28 14:36:45 -0400

    3

---
+++ Curso de Java Básico/ejercicio tema 3/ejerciciotema3.java
@@ -0,0 +1,53 @@
+package Ejerciciotema3;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+        String[] nombres = {"a", "b", "c", "d"};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("los nombres son" + (nombres[i]));
+        }
+    }
+}
+
+//con while
+/*
+package Ejerciciotema3;
+
+public class v2 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        String[] nombres = {"a", "b", "c", "d"};
+
+        while (i < nombres.length) {
+            System.out.println("El nombre es "+ nombres[i]);i++;
+        }
+
+    }
+}
+ */
+
+
+
+
+
+ 
+/*
+package com.example;
+
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+
+        //Creamos el array de tipo String
+        String[] texto = new String[]{"Texto 1", "Texto 2", "Texto 3"};
+
+        //Recorremos el array y mostramos su contenido
+        for(int i = 0; i < texto.length; i++) {
+            System.out.print(texto[i]);
+        }
+
+    }
+
+}
+*/
